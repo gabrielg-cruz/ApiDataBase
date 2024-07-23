c391a42691b5563148b399bf1969faa7fa165fa1
Gabriel <gabrielg-cruz@hotmail.com>
2024-07-23 17:00:30 -0300

    feat

---
+++ ApiBase/Services/Interfaces/ILivrosService.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiBase.Services.Interfaces
+{
+    public interface ILivrosService
+    {
+        //TODO: Implementar métodos de Livros aqui
+    }
+}
